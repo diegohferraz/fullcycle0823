783ea6378b549cdfb760bf715412b48e8b5c7fac
Diego Ferraz <diego_ferraz@mckinsey.com>
2023-08-24 12:23:44 -0300

    feat: alters prisma service to be used globally

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 
+@Global() // Isso vai evitar de eu ter que importar este modulo em todo lugar, ja que vai ficar disponivel globalmente
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports: [PrismaService] // Isso vai permitir que esta service seja usada em outros modulos
 })
-export class PrismaModule {}
+export class PrismaModule { }
--- src/prisma/prisma/prisma.service.ts
+++ src/prisma/prisma/prisma.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  // o OnModule init vai ser executado quando este modulo for carregado no start da aplicacao
+  async onModuleInit() {
+    //o await aqui vai fazer o init da aplicacao esperar pela conexao no banco de dados para continuar
+    await this.$connect();
+  }
+}
