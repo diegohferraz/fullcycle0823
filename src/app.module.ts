41e70485f49e4eff69083e6ab0f018a382aa71b7
Diego Ferraz <diego_ferraz@mckinsey.com>
2023-08-24 12:15:39 -0300

    feat: adds prima module with npx nest generate module prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 // Um modulo eh uma classe que tem este decorator @module ES7 Decorators
 // Este decorator vai registrar o que tem disponivel a minha alicacao
 @Module({
   //Imports posso importar outros modulos
-  imports: [],
+  imports: [PrismaModule],
   //Controlers sao quem habilitam as rodas para receber as requisicoes http
   controllers: [AppController],
   //Camada de servico onde eh recomendado ter as camadas de negocio
