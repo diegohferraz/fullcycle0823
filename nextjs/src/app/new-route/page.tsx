317e2291bc47935b0662b9da9b68dfef8280ea49
Diego Ferraz <diego_ferraz@mckinsey.com>
2023-08-25 16:57:01 -0300

    initial frontend code with search route integration

---
+++ nextjs/src/app/new-route/page.tsx
@@ -0,0 +1,61 @@
+// assim que se informa para o next que este sera um client component
+"use client";
+//o import type vai fazer esse cara ser descartado na compilaçao ja que essa lib só funciona no backend
+import type { FindPlaceFromTextResponseData } from "@googlemaps/google-maps-services-js";
+import { FormEvent } from "react";
+
+const NewRoutePage = () => {
+  const searchPlaces = async (event: FormEvent) => {
+    event.preventDefault();
+    const source = (document.getElementById("source") as HTMLInputElement)
+      .value;
+    const destination = (
+      document.getElementById("destination") as HTMLInputElement
+    ).value;
+
+    const [sourcePlaceRes, destinationPlaceRes] = await Promise.all([
+      fetch(`http://localhost:3000/places?text=${source}`),
+      fetch(`http://localhost:3000/places?text=${destination}`),
+    ]);
+    const [sourcePlace, destinationPlace]: FindPlaceFromTextResponseData[] = await Promise.all([
+      sourcePlaceRes.json(),
+      destinationPlaceRes.json(),
+    ]);
+
+    if (sourcePlace.status !== "OK") {
+      console.error(sourcePlace);
+      alert("Não foi possível encontrar a origem")
+      return
+    }
+    if (destinationPlace.status !== "OK") {
+      console.error(destinationPlace);
+      alert("Não foi possível encontrar o destino")
+      return
+    }
+
+    const placeSourceId = sourcePlace.candidates[0].place_id
+    const placeDestinationId = destinationPlace.candidates[0].place_id
+    
+    const directionsResponse = await fetch(`http://localhost:3000/directions?originId=${placeSourceId}&destinationId=${placeDestinationId}`)
+    const directionsData = await directionsResponse.json()
+  };
+
+  return (
+    <div>
+      <h1>Nova Rota</h1>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={searchPlaces}
+      >
+        <div>
+          <input id="source" type="text" placeholder="origem" />
+        </div>
+        <div>
+          <input id="destination" type="text" placeholder="destino" />
+        </div>
+        <button type="submit">Buscar</button>
+      </form>
+    </div>
+  );
+};
+export default NewRoutePage;
